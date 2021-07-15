c2e9addfbecb0f2be9d66070e200d01bd0c82666
Daniil Sherstnev <daniilsherstnev@Daniils-MacBook-Air.local>
2021-07-15 10:02:52 -0400

    mobile navbar + minor fixes

--- public/js/viewsCounter.js
+++ public/js/viewsCounter.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
 
-let mongoDBConnectionString = process.env.MONGO_URL;
+let mongoDBConnectionString =
+  "mongodb+srv://Daniil:AfpwY9cNymNDPHLo@cluster0.6baqj.mongodb.net/Data?retryWrites=true&w=majority";
+// process.env.MONGO_URL;
 
 let Schema = mongoose.Schema;
 
